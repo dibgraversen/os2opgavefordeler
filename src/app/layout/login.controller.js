a43b3a0b7ad0e1ae8c6deedc25e3ae03cdcb1b02
Sune Marcher <sum@miracle.dk>
2015-06-25 20:12:47 +0200

    Added identity provider buttons on login view. Fixed col-md-x selection for main viewport to include user.loggedIn. Removed mock data from topic-router-api.service.js , added getIdentityProviders().

--- src/app/layout/login.controller.js
+++ src/app/layout/login.controller.js
@@ -3,15 +3,23 @@
 
 	angular.module('topicRouter').controller('LoginCtrl', LoginCtrl);
 
-	LoginCtrl.$inject = ['$scope', '$state', '$timeout', '$q', '$modal', 'topicRouterApi'];
+	LoginCtrl.$inject = ['$scope', 'topicRouterApi', 'serverUrl'];
 
-	function LoginCtrl($scope, $state, $timeout, $q, $modal, topicRouterApi) {
+	function LoginCtrl($scope, topicRouterApi, serverUrl) {
 		/* jshint validthis:true */
 		var vm = this;
+
+		$scope.providers = [];
 
 		activate();
 
 		function activate() {
+			topicRouterApi.getIdentityProviders().then(function(providers) {
+				$scope.providers = providers;
+				_.each($scope.providers, function(provider) {
+					provider.url = serverUrl + "/auth/providers/" + provider.id + "/signin";
+				});
+			});
 		}
 	}
 })();
--- src/app/services/topic-router-api.service.js
+++ src/app/services/topic-router-api.service.js
@@ -10,6 +10,7 @@
 	// NOTE consider implementing ngResource to manage RESTful resource endpoints.
 	function topicRouterApi($http, $q, $timeout, $cacheFactory, serverUrl, appSpinner) {
 		var service = {
+			getIdentityProviders: getIdentityProviders,
 			getUserInfo: getUserInfo,
 			getTopicRoutes: getTopicRoutes,
 			getRoles: getRoles,
@@ -34,6 +35,10 @@
 		var cache = $cacheFactory('cache');
 
 		return service;
+
+		function getIdentityProviders() {
+			return httpGet("/auth/providers");
+		}
 
 		function getUserInfo() {
 			return httpGet('/user/info');
@@ -277,199 +282,6 @@
 						appSpinner.hideSpinner();
 						console.log('**response from EXECUTE', reason);
 					});
-		}
-
-		// mock functions
-
-		function mockTopicRoutes() {
-			var users = getUsers();
-			var digiJon = users[4];
-			var allan = users[1];
-			var jon = users[2];
-
-
-			return [
-				{
-					id: 1,
-					kle: createKLE('01', 'Fysisk planlægning og naturbeskyttelse',
-							'main', 'Dette er en servicetekst til 01.'
-					),
-					rule: {
-						orgUnit: 'Udvikling',
-						externalUnit: 'Plan, udvikling og kultur'
-					},
-					responsible: {
-						id: 2,
-						name: 'Kultur Planlægning og Erhverv (Kenneth Jensen)'
-					},
-					children: [2],
-					open: true,
-					visible: true
-				}, {
-					id: 2,
-					kle: {
-						number: '01.06',
-						name: 'Geografiske informationssystemer',
-						type: 'group',
-						serviceText: 'Dette er en servicetekst til 01.06.'
-					},
-					orgUnit: 'It',
-					externalUnit: 'Digitalisering og It',
-					employee: allan,
-					responsible: {
-						id: 2,
-						name: 'Kultur Planlægning og Erhverv (Kenneth Jensen)'
-					},
-					children: [3, 4],
-					open: true,
-					visible: true
-				}, {
-					id: 3,
-					kle: {
-						number: '01.06.00',
-						name: 'Geografiske informationssystemer i almindelighed',
-						type: 'topic'
-					},
-					orgUnit: 'Digitalisering',
-					externalUnit: 'Digitalisering og It',
-					employee: allan,
-					responsible: digiJon,
-					visible: true
-				}, {
-					id: 4,
-					kle: {
-						number: '01.06.01',
-						name: 'Fikspunkter',
-						type: 'topic'
-					},
-					orgUnit: 'Digitalisering',
-					externalUnit: 'Digitalisering og It',
-					employee: allan,
-					responsible: digiJon,
-					visible: true
-				}, {
-					id: 5,
-					kle: {
-						number: '85',
-						name: 'Kommunens administrative systemer',
-						type: 'main',
-						serviceText: 'Dette er en servicetekst til 85.'
-					},
-					orgUnit: 'Digitalisering',
-					externalUnit: 'Digitalisering og It',
-					employee: jon,
-					responsible: {
-						id: 3,
-						name: 'HR og Digitalisering (Eva Due)'
-					},
-					children: [6],
-					open: true,
-					visible: true
-				}, {
-					id: 6,
-					kle: {
-						number: '85.04',
-						name: 'Blanketter og formularer',
-						type: 'group'
-					},
-					orgUnit: 'Digitalisering',
-					externalUnit: 'Digitalisering og It',
-					employee: jon,
-					responsible: digiJon,
-					children: [7, 8],
-					open: true,
-					visible: true
-				}, {
-					id: 7,
-					kle: {
-						number: '85.04.00',
-						name: 'Blanketter og formularer i almindelighed',
-						type: 'topic'
-					},
-					orgUnit: 'Digitalisering',
-					externalUnit: 'Digitalisering og It',
-					employee: jon,
-					responsible: digiJon,
-					visible: true
-				}, {
-					id: 8,
-					kle: {
-						number: '85.04.02',
-						name: 'KL autoriserede standardblanketter',
-						type: 'topic'
-					},
-					orgUnit: 'Direktionssekretariatet',
-					externalUnit: 'Ledelsessekretariatet',
-					responsible: digiJon,
-					visible: true
-				}
-			];
-		}
-
-		function getMockRoles() {
-			return [
-				{
-					id: 1,
-					employment: 1,
-					name: 'Henrik (dig)',
-					admin: false,
-					municipalityAdmin: false,
-					substitute: false
-				}, {
-					id: 2,
-					name: 'Admin',
-					admin: true,
-					municipalityAdmin: false,
-					substitute: false
-				}, {
-					id: 3,
-					name: 'Jørgen Jensen',
-					admin: false,
-					municipalityAdmin: true,
-					substitute: true
-				}, {
-					id: 4,
-					name: 'Hans Jørgensen',
-					admin: true,
-					municipalityAdmin: true,
-					substitute: true
-				}
-			];
-		}
-
-		function getUsers() {
-			return {
-				4: {
-					id: 1,
-					name: 'Digitalisering (Jon Badstue Pedersen)'
-				},
-				1: {
-					id: 4,
-					name: 'Allan Gyldendal Frederiksen'
-				},
-				2: {
-					id: 5,
-					name: 'Jon Badstue Pedersen'
-				}
-			};
-		}
-
-		function getOrganisations() {
-			return {
-				1: "Udvikling",
-				2: "Direktionssekretariatet",
-				3: "It",
-				4: "Digitalisering"
-			};
-		}
-
-		function createKLE(number, name, type, serviceText) {
-			return {
-				number: number,
-				name: name,
-				type: type,
-				serviceText: serviceText
-			};
 		}
 	}
 })();
