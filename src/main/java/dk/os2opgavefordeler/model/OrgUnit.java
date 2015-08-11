94657e3711f7a03900077a526da441c7eeeda0c7
Henrik Loevborg <hlo@miracleas.dk>
2015-08-11 10:39:58 +0200

    added email and phone to org data and toString

--- src/main/java/dk/os2opgavefordeler/model/OrgUnit.java
+++ src/main/java/dk/os2opgavefordeler/model/OrgUnit.java
@@ -184,9 +184,11 @@ public class OrgUnit implements Serializable, IHasChildren<OrgUnit> {
 	@Override
 	public String toString() {
 		return MoreObjects.toStringHelper(this)
-			.add("id", id)
-			.add("name", name)
-			.toString();
+				.add("id", id)
+				.add("name", name)
+				.add("email", email)
+				.add("phone", phone)
+				.toString();
 	}
 
 	//--------------------------------------------------------------------------
