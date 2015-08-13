0e0136593e6509e834f398d76bbc194a330d50e2
Henrik Loevborg <hlo@miracleas.dk>
2015-08-13 14:14:44 +0200

    bumped version

--- src/app/constants.js
+++ src/app/constants.js
@@ -2,8 +2,8 @@
 	'use strict';
 	angular.module("app.config", [])
 
-.constant("serverUrl", "http://localhost:8080/TopicRouter/rest")
+.constant("serverUrl", "http://topicrouter.miracle.dk/rest")
 
-.constant("version", "0.9.9-SNAPSHOT")
+.constant("version", "0.9.13")
 
 ;})();
