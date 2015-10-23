a61fe17183fce496b59d90cbc5cf4d17bc4726b9
Kresten Kjær <kkj@miracle.dk>
2015-10-23 08:51:44 +0200

    Added puppet setup of server and Vagrant file.

---
+++ environment/puppet/manifests/site.pp
@@ -0,0 +1,157 @@
+node default {
+  include my_os
+  include my_postgresql
+  include my_apache
+  include my_wildfly
+}
+
+# Operating Sytem settings
+class my_os {
+
+  host{'localhost':
+    ip           => "127.0.0.1",
+    host_aliases => ['localhost.localdomain',
+                     'localhost4',
+                     'localhost4.localdomain4'],
+  }
+
+  service { iptables:
+    enable    => false,
+    ensure    => false,
+    hasstatus => true,
+  }
+
+  $install = ['binutils.x86_64','wget', 'java-1.8.0-openjdk-devel']
+
+  package { $install:
+    ensure  => present,
+  }
+}
+
+class my_apache {
+  contain my_os
+
+  class { 'apache':
+    default_mods        => true,
+    default_confd_files => true,
+  }
+
+  apache::mod { 'proxy_ajp': }
+
+  apache::vhost { 'localhost':
+    vhost_name       => '*',
+    port             => '81',
+    docroot          => '/var/www/petshop',
+    proxy_pass => [
+      { 'path' => '/petshop', 'url' => 'ajp://localhost:8009' },
+    ],
+  }
+}
+
+class my_postgresql {
+  contain my_os
+
+  class { 'postgresql::server':
+    ip_mask_allow_all_users    => '0.0.0.0/0',
+    listen_addresses           => '*',
+    postgres_password          => 'password',
+  }
+
+  postgresql::server::db { 'topicrouter':
+    user     => 'topicrouter',
+    password => postgresql_password('topicrouter', 'SuperSaltFisk'),
+  }
+
+  postgresql::server::role { 'topicrouter_managers':
+    password_hash => postgresql_password('topicrouter_managers', 'SuperSaltFisk'),
+  }
+
+  postgresql::server::database_grant { 'test1':
+    privilege => 'ALL',
+    db        => 'topicrouter',
+    role      => 'topicrouter_managers',
+  }
+
+  postgresql::validate_db_connection { 'validate my postgres connection':
+    database_host           => 'localhost',
+    database_username       => 'topicrouter',
+    database_password       => 'SuperSaltFisk',
+    database_name           => 'topicrouter',
+    require                 => Postgresql::Server::Db['topicrouter']
+  }
+}
+
+
+class my_wildfly{
+  contain my_os, my_postgresql
+
+  class { 'wildfly':
+    version           => '8.2.0',
+    install_source    => 'http://download.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.tar.gz',
+    java_home         => '/usr/lib/jvm/java-1.8.0-openjdk',
+    dirname           => '/opt/wildfly',
+    mode              => 'standalone',
+    config            => 'standalone-full-ha.xml',
+    users_mgmt        => { 'wildfly' => { username => 'wildfly', password => 'wildfly'}},
+  }
+
+  wildfly::deploy { 'sample.war':
+    source   => 'https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war',
+    require  => Class['wildfly'],
+  }
+
+  wildfly::config::add_mgmt_user { 'Adding mgmtuser':
+    username => 'mgmtuser',
+    password => 'mgmtuser',
+    require  => Class['wildfly'],
+  }
+
+  wildfly::config::add_app_user { 'Adding appuser':
+    username => 'appuser',
+    password => 'appuser',
+    require  => Class['wildfly'],
+  }
+
+  wildfly::config::associate_groups_to_user { 'Associate groups to mgmtuser':
+    username => 'mgmtuser',
+    groups   => 'admin,mygroup',
+    require  => Wildfly::Config::Add_mgmt_user['Adding mgmtuser'],
+  }
+
+  wildfly::config::associate_roles_to_user { 'Associate roles to app user':
+    username => 'appuser',
+    roles    => 'guest,ejb',
+    require  => Wildfly::Config::Add_app_user['Adding appuser'],
+  }
+
+  wildfly::messaging::queue { 'DemoQueue':
+    durable => true,
+    entries => ['java:/jms/queue/DemoQueue'],
+    require => Class['wildfly'],
+  }
+
+  wildfly::messaging::topic { 'DemoTopic':
+    entries => ['java:/jms/topic/DemoTopic'],
+    require => Class['wildfly'],
+  }
+
+  wildfly::config::module { 'org.postgresql':
+    source       => 'http://central.maven.org/maven2/org/postgresql/postgresql/9.3-1103-jdbc4/postgresql-9.3-1103-jdbc4.jar',
+    dependencies => ['javax.api', 'javax.transaction.api'],
+    require      => Class['wildfly'],
+  } ->
+  wildfly::datasources::driver { 'Driver postgresql':
+    driver_name                     => 'postgresql',
+    driver_module_name              => 'org.postgresql',
+    driver_xa_datasource_class_name => 'org.postgresql.xa.PGXADataSource'
+  } ->
+  wildfly::datasources::datasource { 'OS2TopicRouterDS datasource':
+    name           => 'OS2TopicRouterDS',
+    config         => { 'driver-name'    => 'postgresql',
+                        'connection-url' => 'jdbc:postgresql://localhost/topicrouter',
+                        'jndi-name'      => 'java:/OS2TopicRouterDS',
+                        'user-name'      => 'topicrouter',
+                        'password'       => 'SuperSaltFisk'
+                      }
+  }
+}
---
+++ environment/puppet/modules/wildfly/manifests/prepare.pp
@@ -0,0 +1,43 @@
+#
+# Wildlfy prepare class
+#
+class wildfly::prepare {
+
+  if $wildfly::manage_user {
+
+    group { $wildfly::group :
+      ensure => present,
+    }
+
+    user { $wildfly::user :
+      ensure     => present,
+      groups     => $wildfly::group,
+      shell      => '/bin/bash',
+      home       => "/home/${wildfly::user}",
+      comment    => "${wildfly::user} user created by Puppet",
+      managehome => true,
+      require    => Group[$wildfly::group],
+    }
+  }
+
+  file { $wildfly::dirname :
+    ensure  => directory,
+    owner   => $wildfly::user,
+    group   => $wildfly::group,
+    mode    => '0755',
+    require => User[$wildfly::user],
+  }
+
+  $libaiopackage  = $::osfamily ? {
+    'RedHat' => 'libaio',
+    'Debian' => 'libaio1',
+    default  => 'libaio',
+  }
+
+  if !defined(Package[$libaiopackage]) {
+    package { $libaiopackage:
+      ensure => present,
+    }
+  }
+
+}
