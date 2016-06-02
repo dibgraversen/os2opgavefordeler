6ed9aeb4c0cb96576261b6e10874c7eb1236ca17
Rune Nielsen <rni@miracle.dk>
2016-06-02 14:14:32 +0200

    Added validation for extended responsibility rules

--- sources/frontend/src/main/webapp/app/home/extended-responsibility.controller.js
+++ sources/frontend/src/main/webapp/app/home/extended-responsibility.controller.js
@@ -12,6 +12,8 @@
         $scope.removeFilter = removeFilter;
         $scope.add = add;
         $scope.currentTab = 'list';
+	    $scope.ruleAlerts = [];
+	    $scope.initialType = '';
 
         function cleanModel() {
 	        $log.info('ExtendedResponsibilityController::cleanModel (type: ' + $scope.type + ')');
@@ -24,6 +26,8 @@
 
 	        $scope.type = defaultFilterType;
 
+	        $scope.ruleAlerts = [];
+
 	        activateTab($scope.type); // select the correct tab based on filter type
 
             return {
@@ -34,7 +38,11 @@
 
         $scope.model = {
             distributionRuleId: $scope.topic.id,
-            type: $scope.type
+            type: $scope.type,
+	        name: '',
+	        days: '',
+	        months: '',
+	        text: ''
         };
 
         var orgUnitsMissing = true;
@@ -49,6 +57,7 @@
         $scope.loadMoreEmployments = loadMoreEmployments;
         $scope.setSelectedEmp = setSelectedEmp;
         $scope.show = show;
+	    $scope.closeAlert = closeAlert;
 
         $scope.search = {
             municipalityId: $scope.user.municipality.id,
@@ -76,11 +85,14 @@
         function show(filterId, filterType){
             $log.info("ExtendedResponsibilityController::show filter " + filterId + " (type: " + filterType + ")");
 
-            $scope.model = cleanModel();
+	        $scope.ruleAlerts = [];
+
+	        $scope.model = cleanModel();
             $scope.currentTab = 'show';
             $scope.model.filterId = filterId;
 	        $scope.model.type = filterType;
 	        $scope.type = filterType;
+	        $scope.initialType = filterType;
 
 	        activateTab(filterType); // select the correct tab based on filter type
 
@@ -102,18 +114,100 @@
         function updateFilter(){
             $log.info("ExtendedResponsibilityController::updateFilter");
 
-            topicRouterApi.updateFilter($scope.model).then(function() {
-                _refresh();
-                $scope.currentTab = 'list'
-            });
+	        setFilterFromModel();
         }
 
         function createFilter(){
-            topicRouterApi.createFilter($scope.model).then(function(){
-                _refresh();
-                $scope.currentTab = 'list'
-            });
+	        $log.info("ExtendedResponsibilityController::createFilter (initial type was '" + $scope.initialType + "', new type is: '" + $scope.model.type + "')");
+
+			if ($scope.model.name) { // the name must be supplied
+				if ($scope.selectedOrgUnit.id) { // the user must select an organisational unit
+					if ($scope.initialType === '' || $scope.initialType === $scope.model.type) {
+						if ($scope.model.type === 'cpr') { // make sure the user has provided days and/or months, if needed
+							if ($scope.model.days || $scope.model.months) {
+								// check that the numbers specified for days and/or months are valid
+								var validDays = true;
+								var validMonths = true;
+
+								if ($scope.model.days) {
+									validDays = validateDays($scope.model.days);
+								}
+
+								if ($scope.model.months) {
+									validMonths = validateMonths($scope.model.months);
+								}
+
+								if (validDays && validMonths) {
+									setFilterFromModel();
+								}
+								else {
+									if (!validDays) {
+										addAlert({
+											type: 'warning',
+											msg: 'Der er fejl i de angivne dage.'
+										});
+									}
+
+									if (!validMonths) {
+										addAlert({
+											type: 'warning',
+											msg: 'Der er fejl i de angivne måneder.'
+										});
+									}
+								}
+							}
+							else {
+								addAlert({
+									type: 'warning',
+									msg: 'Du skal angive dage og/eller måneder.'
+								});
+							}
+						}
+						else if ($scope.type = 'text') {
+							if ($scope.model.text) {
+								setFilterFromModel();
+							}
+							else {
+								addAlert({
+									type: 'warning',
+									msg: 'Du skal angive en tekst.'
+								});
+							}
+						}
+						else {
+							$log.error('Unknown filter type (valid values: cpr, text)');
+						}
+					}
+					else {
+						addAlert({
+							type: 'warning',
+							msg: 'Det er ikke muligt at ændre typen for en eksisterende regel.'
+						});
+					}
+				}
+				else {
+					addAlert({
+						type: 'warning',
+						msg: 'Du skal som minimum vælge en organisatorisk enhed.'
+					});
+				}
+			}
+	        else {
+				addAlert({
+					type: 'warning',
+					msg: 'Du skal angive et navn for den udvidede fordelingsregel.'
+				});
+			}
         }
+
+	    function setFilterFromModel() {
+		    $log.info('ExtendedResponsibilityController::setFilterFromModel (type: ' + $scope.model.type + ')');
+
+		    topicRouterApi.createFilter($scope.model).then(function() {
+			    _refresh();
+			    $scope.currentTab = 'list'
+		    });
+	    }
 
         function setSelectedEmp(emp){
 	        $scope.selectedEmp = emp;
@@ -197,6 +291,9 @@
 
         function add() {
             $scope.model = cleanModel();
+
+	        $scope.initialType = '';
+
             $scope.currentTab = "add";
         }
 
@@ -212,8 +309,82 @@
 
         function close() {
             $log.info("ExtendedResponsibilityController::close");
+
 	        $modalStack.dismissAll('cancel');
         }
 
+	    function addAlert(alert) {
+		    $scope.ruleAlerts.push(alert);
+	    }
+
+	    function closeAlert(index) {
+		    $scope.ruleAlerts.splice(index, 1);
+	    }
+
+	    function isInt(value) {
+		    return !isNaN(value) &&
+				    parseInt(Number(value)) == value &&
+				    !isNaN(parseInt(value, 10));
+	    }
+
+	    function isValidDay(value) {
+		    return isInt(value) && parseInt(Number(value)) > 0 && parseInt(Number(value)) < 32;
+	    }
+
+	    function isValidMonth(value) {
+		    return isInt(value) && parseInt(Number(value)) > 0 && parseInt(Number(value)) < 13;
+	    }
+
+	    function validateDays(input) {
+		    var dayStrings = input.split(',');
+
+		    for (var i = 0; i < dayStrings.length; i++) {
+			    if (dayStrings[i].indexOf('-') > -1) { // interval
+					var dayArr = dayStrings[i].split('-');
+
+				    if (dayArr.length == 2) {
+						if (!isValidDay(dayArr[0]) || !isValidDay(dayArr[1])) {
+							return false;
+						}
+				    }
+				    else {
+					    return false;
+				    }
+			    }
+			    else { // single day
+				    if (!isValidDay(dayStrings[i])) { // one or more days are invalid
+					    return false;
+				    }
+			    }
+		    }
+
+		    return true;
+	    }
+
+	    function validateMonths(input) {
+		    var monthsStrings = input.split(',');
+
+		    for (var i = 0; i < monthsStrings.length; i++) {
+			    if (monthsStrings[i].indexOf('-') > -1) { // interval
+				    var monthArr = monthsStrings[i].split('-');
+
+				    if (monthArr.length == 2) {
+					    if (!isValidMonth(monthArr[0]) || !isValidMonth(monthArr[1])) {
+						    return false;
+					    }
+				    }
+				    else {
+					    return false;
+				    }
+			    }
+			    else { // single day
+				    if (!isValidMonth(monthsStrings[i])) { // one or more days are invalid
+					    return false;
+				    }
+			    }
+		    }
+
+		    return true;
+	    }
     }
 })();
