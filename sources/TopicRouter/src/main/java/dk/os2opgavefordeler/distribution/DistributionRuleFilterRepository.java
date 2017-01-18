9d710b9b53805a3afa86c06b42b7c82f30898f4e
Henrik Loevborg <hlo@miracle.dk>
2017-01-18 13:15:16 +0100

    extra lookup methods

--- sources/TopicRouter/src/main/java/dk/os2opgavefordeler/distribution/DistributionRuleFilterRepository.java
+++ sources/TopicRouter/src/main/java/dk/os2opgavefordeler/distribution/DistributionRuleFilterRepository.java
@@ -1,9 +1,16 @@
 package dk.os2opgavefordeler.distribution;
 
+import dk.os2opgavefordeler.model.DistributionRule;
 import dk.os2opgavefordeler.model.DistributionRuleFilter;
+import dk.os2opgavefordeler.model.Employment;
+import dk.os2opgavefordeler.model.OrgUnit;
 import org.apache.deltaspike.data.api.AbstractEntityRepository;
+import org.apache.deltaspike.data.api.Modifying;
+import org.apache.deltaspike.data.api.Query;
 import org.apache.deltaspike.data.api.Repository;
 import org.apache.deltaspike.jpa.api.transaction.Transactional;
+
+import java.util.List;
 
 /**
  * @author hlo@miracle.dk
@@ -11,4 +18,17 @@ import org.apache.deltaspike.jpa.api.transaction.Transactional;
 @Repository(forEntity = DistributionRuleFilter.class)
 @Transactional
 public abstract class DistributionRuleFilterRepository extends AbstractEntityRepository<DistributionRuleFilter, Long> {
+
+	@Query("select filter from DistributionRuleFilter filter where filter.distributionRule in ( ?1 )")
+	public abstract List<DistributionRuleFilter> findForRules(List<DistributionRule> rules);
+
+	@Query("select filter from DistributionRuleFilter filter where filter.assignedOrg in ( ?1 )")
+	public abstract List<DistributionRuleFilter> findByAssignedOrg(List<OrgUnit> orgs);
+
+	@Modifying
+	@Query("update DistributionRuleFilter filter set filter.assignedEmp = null where filter.assignedEmp in ( ?1 )")
+	public abstract void unsetEmployments(List<Employment> employments);
+
+	@Query("select filter from DistributionRuleFilter filter where filter.assignedEmp is null and filter.assignedOrg is null")
+	public abstract List<DistributionRuleFilter> findAbandoned();
 }
