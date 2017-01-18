091bdc8c6132c1ce09f754afa199d9e40955b3d8
Henrik Loevborg <hlo@miracle.dk>
2017-01-18 09:10:35 +0100

    deprecated methods doing absolutely nothing

--- sources/TopicRouter/src/main/java/dk/os2opgavefordeler/service/AuthorizationService.java
+++ sources/TopicRouter/src/main/java/dk/os2opgavefordeler/service/AuthorizationService.java
@@ -3,6 +3,6 @@ package dk.os2opgavefordeler.service;
 import dk.os2opgavefordeler.model.Role;
 
 public interface AuthorizationService {
-	void verifyIsAdmin() throws AuthorizationException;
-	void verifyCanActAs(Role role) throws AuthorizationException;
+	@Deprecated void verifyIsAdmin() throws AuthorizationException;
+	@Deprecated void verifyCanActAs(Role role) throws AuthorizationException;
 }
