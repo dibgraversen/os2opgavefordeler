e13d4bafe8d834958bbd422bf60e2c9a39d5deaf
Daniel Torres <dto@digital-identity.dk>
2017-02-03 15:00:05 +0100

    updated ui to use tree structure and call some rest endpoints

--- sources/frontend/src/main/webapp/app/kle-admin/kle-admin.controller.js
+++ sources/frontend/src/main/webapp/app/kle-admin/kle-admin.controller.js
@@ -12,91 +12,87 @@
 		$scope.filterStr = "";
 		$scope.$log=$log;
 
-		$scope.kles = [];
-		$scope.displayOus = [];
+		$scope.ous = [];
 		$scope.restKles = [];
 
 
 		$scope.setCurrentOrgUnit = setCurrentOrgUnit;
-		$scope.getDisplayKlesFromScope = getDisplayKlesFromScope;
 		$scope.modifyKle = modifyKle;
 
 		activate();
 
 		function activate() {
-			topicRouterApi.getMunicipalities().then(function(municipalities){
-				$scope.municipalities = municipalities;
+			orgUnitService.getRestKles().then(function(kles){
+				$scope.restKles = kles;
 			});
 
-			orgUnitService.getKLEs().then(function(kles){
-				$scope.kles = kles;
-			});
-
-			orgUnitService.getOrgUnits().then(function(orgUnits){
-				setCurrentOrgUnit(orgUnits[0]);
-				initDisplayObjects(orgUnits);
-
-			});
-			getRestKles();
+			orgUnitService.getOrgUnits().then(function(ous){
+				$scope.ous = JSON.parse(JSON.stringify(ous));
+				
+				// add 'displayKles' to the ous (this maps to the checkboxes in the ui)
+				for(var i = 0; i < $scope.ous.length; i++){
+					$scope.ous[i].displayKles = JSON.parse(JSON.stringify($scope.restKles));			
+				}
+				setCurrentOrgUnit($scope.ous[0]);				
+				});		
 		}
 
-		function initDisplayObjects(ous) {
-			for (var i = 0; i < ous.length; i++) {
-				var ouWithDisplayData = ous[i];
-				ouWithDisplayData.displayKles = getDisplayKles(ous[i]);
-				$scope.displayOus.push(ouWithDisplayData);				
-			}	
+		function setCurrentOrgUnit(ou){
+			$scope.currentOrgUnit = ou;		
 		}
 
-		function getDisplayKles(orgunit) {
-			var klesWithDisplayData = [];
-			
-			 for (var i = 0; i < $scope.kles.length; i++){
-				var newKle= JSON.parse(JSON.stringify($scope.kles[i]));
-				newKle.checked = orgUnitService.containsKle($scope.kles[i],orgunit,$scope.kles[i].assignmentType);
-				klesWithDisplayData.push(newKle);				
+		function isKleAssigned(orgunit, kleNumber){
+			for(var i = 0; i<orgunit.kles.length; i++){
+				if(orgunit.kles[i].number==kleNumber){
+					return true;
+				}
 			}
-			
-			return klesWithDisplayData;
-		}
-		
-
-		function getDisplayKlesFromScope(orgunit) {	
-			for (var i = 0; i < $scope.displayOus.length; i++){
-				if(orgunit.id== $scope.displayOus[i].id) {
-					return orgunit.id;
-				}				
-			}
-			
-			return null;
-		}
-
-		function getRestKles(){
-			orgUnitService.getRestKles().then( function(restKles){
-				$scope.restKles = restKles;
-			});
-		}
-
-		function setCurrentOrgUnit(orgUnit){
-			$scope.currentOrgUnit = orgUnit;
+			return false;
 		}
 
 		function expand(kle){
 			kle.expanded = true;
 		}
 
-		function modifyKle(checked,kle,orgunit){
+		function modifyKle(checked,kle,ou){
 			if(checked){
-					orgUnitService.addKle(kle,orgunit).then(function(){
-													console.log("added kle");
+					orgUnitService.addKle(kle,ou).then(function(){
+													console.log("assigned kle '" + kle.number + "' to OrgUnit '" +  ou.name + "'");
+													addKle(ou,kle);	// updates local ou model 
+													kle.assigned=true;
 												});
 			}
 			else {
-				orgUnitService.removeKle(kle,orgunit).then(function(){
-													console.log("removed kle");
+				orgUnitService.removeKle(kle,ou).then(function(){
+													console.log("unassigned kle '" + kle.number + "' to OrgUnit '" +  ou.name + "'");
+													removeKle(ou,kle); // updates local ou model 
+													kle.assigned=false;
 												});
 			}
 					
+		}
+
+		function addKle(ou,kle){
+			for(var i; i < $scope.ous.length; i++){
+				if($scope.ous[i].id == ou.id){
+					$scope.ous[i].kles.push(kle);
+				}
+			}
+
+			for(var z; z < ou.displayKles.length; z++){
+				if(ou.displayKles[z].number == kle.number){
+					ou.displayKles[z].assigned=true;
+				}
+			}
+
+		}
+
+		function removeKle(ou,kle){
+			for(var i; i < $scope.ous.length; i++){
+				if($scope.ous[i].id == ou.id){
+					$scope.ous[i].kles.splice(i,1);
+				}
+			}
 		}
 	}
 })();
--- sources/frontend/src/main/webapp/app/services/orgunit.service.js
+++ sources/frontend/src/main/webapp/app/services/orgunit.service.js
@@ -9,39 +9,37 @@
 	function orgUnitService($http, $q, $timeout, $cacheFactory, serverUrl, appSpinner, $log) {		
 		var service = {
 			getOrgUnits: getOrgUnits,
-			getKLEs: getKLEs,
-			containsKle: containsKle,
 			addKle : addKle,
 			removeKle : removeKle,
 			getRestKles : getRestKles
 		};
 
+		var baseUrl = serverUrl;
+
+		var requestConfig = {
+			headers: {
+				'Content-Type': 'application/json',
+				'Cache-Control': 'no-cache',
+				'Pragma': 'no-cache',
+				'Expires': '-1'
+			}
+		};
 		return service;
 
 		function getOrgUnits() {
 			var getOrgUnits = function() {
 			 	 var ou1 = new OrgUnit('1','2','3','Aarhus Kommune', '2', 'aa@aarhus.dk', '1234565789',
 						   			 	[
-						   			 		new KLEExtended (new KLE(1,"01.01.01", "Kontante ydelses"), 'AssignmentType1'),
-						   			 		new KLEExtended (new KLE(2,"01.02.02", "Sociale pensioner"), 'AssignmentType2')
+						   			 		new KLEExtended (new KLE(1,"00", "Kontante ydelses"), 'AssignmentType1'),
+						   			 		new KLEExtended (new KLE(2,"13", "Sociale pensioner"), 'AssignmentType2')
 						   			 	]);
 			 	 var ou2 = new OrgUnit('2','20','30','Aalborg Kommune', '3', 'alborg@aalborg.dk','1234565789',
 						   			 	[
-						   			 		new KLEExtended (new KLE(3,"01.02.02", "Sociale pensioner"), 'AssignmentType1'),
+						   			 		new KLEExtended (new KLE(3,"00", "Sociale pensioner"), 'AssignmentType1'),
 						   			 	]);
        			 return[ou1,ou2];
        			};   		
 			return simulateRestCall(getOrgUnits);
-		}
-
-		function getKLEs(){
-			var getKles = function() {
-				var kle1 = new KLEExtended (new KLE(1,"01.01.01", "Kontante ydelses"), 'AssignmentType1');
-				var kle2 = new KLEExtended (new KLE(2,"01.02.02", "Sociale pensioner"), 'AssignmentType2');
-				var kle3 = new KLEExtended (new KLE(2,"01.02.02", "Sociale pensioner"), 'AssignmentType1');
-	 				return[kle1,kle2,kle3];
-				};
-			return simulateRestCall(getKles)
 		}
 
 		function addKle(kle,orgunit){
@@ -59,26 +57,6 @@
 		function simulateRestCall(functionToSimulate){ 
 			return $timeout(functionToSimulate,10);
 		}
-
-		function containsKle(kle,orgUnit,assignmentType) {
-			if (orgUnit == null){
-				return false;
-			}
-
-			if (orgUnit.kles == null){
-				return false;
-			}
-
- 		   for(var i = 0; i < orgUnit.kles.length; i++){
-				if( orgUnit.kles[i].kle.id == kle.id && assignmentType == orgUnit.kles[i].assignmentType){
-					return true;
-				}
-			}
-
-
-			return false;
-		}
-
 
 		function OrgUnit(id, parentId, managerId, name, esdhId, email, phone, kles) {
 			return {
@@ -122,7 +100,8 @@
 
 
 	function getRestKles(){
-		var simulateCallToGetKles = function (){
+	/*
+		var func = function(){
 				return { "KLE": [
 				  {
 				    "number": "00",
@@ -238,8 +217,46 @@
 				      }
 				    ]
 				  }
-				]}};
-			return simulateRestCall(simulateCallToGetKles);
+				]};
+			};
+			return simulateRestCall(func);
+			*/			
+			return httpGet('/kle2');
+
 		} // end of getKleRestService
+
+		function httpGet(url, params) {
+			var options = {
+				//cache: cache
+			};
+			if (params) {
+				//options.params = encodeURIComponent( JSON.stringify(params) );
+				options.params = params;
+			}
+			return httpExecute(url, 'GET', options);
+		}
+
+		function httpExecute(requestUrl, method, options) {
+			var defaults = {
+				url: baseUrl + requestUrl,
+				method: method,
+				withCredentials: true,
+				headers: requestConfig.headers
+			};
+
+			$log.log(defaults.url);
+			
+			angular.extend(options, defaults); // merge defaults into options.
+			appSpinner.showSpinner();
+			return $http(options).then(
+					function (response) {
+						appSpinner.hideSpinner();
+						return response.data;
+					},
+					function (reason) {
+						appSpinner.hideSpinner();
+						return $q.reject(reason);
+					});
+		}
 	}
 })();
