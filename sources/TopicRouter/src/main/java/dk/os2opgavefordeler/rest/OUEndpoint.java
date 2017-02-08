c0a0b1609d146dd5eaf3fd81aeffdb647ef0bdd9
Piotr Suski <psu@digital-identity.dk>
2017-02-08 14:46:31 +0100

    Small fixes

--- sources/TopicRouter/src/main/java/dk/os2opgavefordeler/rest/OUEndpoint.java
+++ sources/TopicRouter/src/main/java/dk/os2opgavefordeler/rest/OUEndpoint.java
@@ -3,6 +3,7 @@ package dk.os2opgavefordeler.rest;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -15,9 +16,8 @@ import dk.os2opgavefordeler.auth.KleAssignerRequired;
 import dk.os2opgavefordeler.model.presentation.OrgUnitWithKLEPO;
 import dk.os2opgavefordeler.service.OrgUnitWithKLEService;
 
-//ApplicationScoped for test only. Should be RequestScoped with ApplicationScoped repository.
 @Path("/ou")
-@ApplicationScoped
+@RequestScoped
 public class OUEndpoint extends Endpoint {
 
 	@Inject
@@ -42,54 +42,5 @@ public class OUEndpoint extends Endpoint {
 			return Response.status(404).entity("Entity not found for ID: " + id).build();
 		}
 	}
-
-	// @POST
-	// @Path("/")
-	// @Consumes(MediaType.APPLICATION_JSON)
-	// @Produces(MediaType.APPLICATION_JSON)
-	// public Response post(OURestResultPO input) {
-	// ous.add(input);
-	// return Response.ok().build();
-	// }
-	//
-	// @POST
-	// @Path("/{Id}/kle/{kleNumber}")
-	// @Consumes(MediaType.APPLICATION_JSON)
-	// @Produces(MediaType.APPLICATION_JSON)
-	// public Response addKLE(@PathParam("Id") long id,@PathParam("kleNumber")
-	// String kleNumber, JSONObject input) {
-	// //Gather data
-	// Optional<OURestResultPO> ou = ous.stream().filter(x ->
-	// x.getId()==id).findFirst();
-	// KleRestResultPO kle = findKLE(kles, kleNumber);
-	// String assignmentType = input.get("assignmentType")+"";
-	// //add kle
-	// if(ou.isPresent() && kle!=null){
-	// ou.get().addKle(kle.getNumber(),assignmentType);
-	// }else{
-	// return Response.status(404).build();
-	// }
-	// return Response.ok().entity(input).build();
-	// }
-	//
-	// @POST
-	// @Path("/{Id}/removeKLE")
-	// @Consumes(MediaType.APPLICATION_JSON)
-	// @Produces(MediaType.APPLICATION_JSON)
-	// public Response removeKLE(@PathParam("Id") long id, JSONObject input) {
-	// //gather
-	// Optional<OURestResultPO> ou = ous.stream().filter(x ->
-	// x.getId()==id).findFirst();
-	// //remove kle
-	// if(ou.isPresent()){
-	// if(input.containsKey("number")){
-	// String number = input.get("number").toString();
-	// ou.get().removeKle(number);
-	// }
-	// }else{
-	// return Response.status(404).build();
-	// }
-	// return Response.ok().entity(input).build();
-	// }
-
+	
 }
